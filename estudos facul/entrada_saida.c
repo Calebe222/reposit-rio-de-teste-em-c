dde6f9801dc50cf2228ef43a0d076179479f3510
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-02-18 14:20:42 +0000

    printf e scanf

--- estudos facul/entrada_saida.c
+++ estudos facul/entrada_saida.c
@@ -7,7 +7,14 @@ int main () {
     char nome[20] = "Calebe";
 
 
-    printf("A idade do %s é: %d\n", nome, idade);
-    printf("A altura é: %f\n", altura);
+    printf("A idade é: %d\n", idade);
+    printf("O nome é: %s\n", nome);
+    printf("A altura é: %.2f\n", altura);
     printf("A opcao é: %c\n", opcao);
+
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Sua idade é: %d", idade);
+
 }
