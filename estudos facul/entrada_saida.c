2e72f57d824a2c6800021aa7c75da27adb83944f
Calebe222 <136852396+Calebe222@users.noreply.github.com>
2025-02-18 14:07:08 +0000

    novas váriveis

---
+++ estudos facul/entrada_saida.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int idade = 25;
+    float altura = 1.80;
+    char opcao = 'C';
+    char nome[20] = "Calebe";
+
+
+    printf("A idade do %s é: %d\n", nome, idade);
+    printf("A altura é: %f\n", altura);
+    printf("A opcao é: %c\n", opcao);
+}
